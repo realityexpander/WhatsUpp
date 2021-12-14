41705908b98fa2a23184c5a3aea5fef3f4a44f4e
Chris Athanas <chris@crowdskout.com>
2021-12-13 22:14:58 -0700

    Add firebase/firestore/multidex

--- app/src/main/java/com/realityexpander/whatsupp/LoginActivity.kt
+++ app/src/main/java/com/realityexpander/whatsupp/LoginActivity.kt
@@ -5,17 +5,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.realityexpander.whatsupp.databinding.ActivityLoginBinding
 import com.realityexpander.whatsupp.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var bind: ActivityLoginBinding
 
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityLoginBinding.inflate(layoutInflater)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(bind.root)
     }
 
@@ -26,6 +31,7 @@ class LoginActivity : AppCompatActivity() {
 
     fun onLogin(v: View) {
         Toast.makeText(v.context, "Log in", Toast.LENGTH_SHORT).show()
+
     }
 
     companion object {

f032aa06216c1cccf00831ecdf8853ca873eded3
Chris Athanas <chris@crowdskout.com>
2021-12-18 14:01:23 -0600

    Remove AsyncTask for StatusActivity

--- app/src/main/java/com/realityexpander/whatsupp/activities/StatusActivity.kt
+++ app/src/main/java/com/realityexpander/whatsupp/activities/StatusActivity.kt
@@ -6,16 +6,22 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.firestore.core.ActivityScope
 import com.realityexpander.whatsupp.databinding.ActivityStatusBinding
 import com.realityexpander.whatsupp.listener.ProgressListener
 import com.realityexpander.whatsupp.util.StatusListItem
 import com.realityexpander.whatsupp.util.loadUrl
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
 
 const val PARAM_STATUS_ELEMENT = "element"
 
-class StatusActivity : AppCompatActivity(), ProgressListener {
+class StatusActivity : AppCompatActivity(), CoroutineScope {
     private lateinit var bind: ActivityStatusBinding
     private lateinit var statusElement: StatusListItem
+
+    override val coroutineContext = Job() + Dispatchers.Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,33 +39,46 @@ class StatusActivity : AppCompatActivity(), ProgressListener {
         bind.statusIv.loadUrl(statusElement.statusUrl)
 
         bind.progressBar.max = 100
-        TimerTask(this).execute("")
+//        TimerTask(this).execute("")
+
+        val errorHandler = CoroutineExceptionHandler {
+            context, throwable->
+        }
+        launch(errorHandler) {
+            withContext(Dispatchers.IO) {
+                for(i in 1..100) {
+                    delay(30)
+                    onProgressUpdate(i)
+                }
+            }
+        }
+
     }
 
-    override fun onProgressUpdate(progress: Int) {
+    private fun onProgressUpdate(progress: Int) {
         bind.progressBar.progress = progress
         if(progress == 100) {
             finish()
         }
     }
 
-    private class TimerTask(val listener: ProgressListener): AsyncTask<String, Int, Any>() {
-        override fun doInBackground(vararg params: String?) {
-            var i = 0
-            val sleep = 30L
-            while (i < 100) {
-                i++
-                publishProgress(i)
-                Thread.sleep(sleep)
-            }
-        }
-
-        override fun onProgressUpdate(vararg values: Int?) {
-            if(values[0] != null) {
-                listener.onProgressUpdate(values[0]!!)
-            }
-        }
-    }
+//    private class TimerTask(val listener: ProgressListener): AsyncTask<String, Int, Any>() {
+//        override fun doInBackground(vararg params: String?) {
+//            var i = 0
+//            val sleep = 30L
+//            while (i < 100) {
+//                i++
+//                publishProgress(i)
+//                Thread.sleep(sleep)
+//            }
+//        }
+//
+//        override fun onProgressUpdate(vararg values: Int?) {
+//            if(values[0] != null) {
+//                listener.onProgressUpdate(values[0]!!)
+//            }
+//        }
+//    }
 
     companion object {
         fun getIntent(context: Context?, statusElement: StatusListItem): Intent {
